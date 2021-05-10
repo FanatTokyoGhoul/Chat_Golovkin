54fc730626703714e994a930aab38dddc771cfad
FanatTokyoGhoul <nayzer322@gmail.com>
2021-05-10 14:58:22 +0300

    commt

---
+++ Klient.c
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <pthread.h>
+#define SIZE_MSG 512
+
+char* print_name;
+
+void error(const char *msg) {
+	perror(msg);
+	exit(1);
+}
+
+static void* klient_handler(void* args){
+    int* serverfd = (int*)args;
+    char msg[SIZE_MSG];
+    printf("Fd: %d\n", *serverfd);
+    while (1){
+
+        if(recv(*serverfd, msg, sizeof(msg), 0) > 0){
+            printf(msg);
+            printf("\n");
+            printf(print_name);
+            bzero(msg, SIZE_MSG);
+        }else{
+            exit(-1);
+        }
+    }
+    
+}
+
+int main(int argc, char *argv[]) {
+
+    printf("Enter name: ");
+    char name[100];
+    fgets(name, sizeof(name), stdin);
+
+    size_t len = strlen(name);
+    if(len > 0 && name[len - 1] == '\n'){
+        name[--len] = '\0';
+    }
+
+	int clie_sockfd, n;
+	char buffer[256];
+	//описывает сокет для работы с протоколами IP
+	struct sockaddr_in serv_addr;
+
+	struct hostent *server;
+
+	clie_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	server = gethostbyname("127.0.0.1");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port = htons(3425); 
+	if(connect(clie_sockfd, (struct sockaddr *) &serv_addr,	sizeof(serv_addr)) < 0)
+		error("Error");
+	
+    print_name = name;
+    strncat(print_name, ": ", 5); 
+
+    pthread_t tid;
+    pthread_create(&tid, NULL, klient_handler, (void *)&clie_sockfd);
+
+    char msg[SIZE_MSG];
+
+    send(clie_sockfd, name, sizeof(msg), 0);
+
+
+    while (1){
+        printf(print_name);
+        bzero(msg, SIZE_MSG);
+        fgets(msg, SIZE_MSG, stdin);
+        send(clie_sockfd, msg, sizeof(msg), 0);
+    }
+    
+
+	close(clie_sockfd);
+	return 0;
+}
---
+++ Server.c
@@ -0,0 +1,110 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <pthread.h>
+#define SIZE_MSG 512
+
+int users_sockfd[100];
+pthread_t pthid[100];
+int connect_users = 0;
+
+void error(const char *msg) {
+	perror(msg);
+	exit(1);
+}
+
+void delete_userfd(int userfd){
+    
+    int index = 0;
+    for(int i = 0; i <= connect_users; i++){
+        if(users_sockfd[i] == userfd){
+            index = i;
+        }
+    }
+
+    for(int i = index;i <= connect_users; i++){
+        users_sockfd[i] = users_sockfd[i+1];
+        pthid[i] = pthid[i+1];
+    }
+
+    pthread_t t;
+    pthid[connect_users] = t;
+    users_sockfd[connect_users] = 0;
+    connect_users--;
+}
+
+static void* server_handler(void* args){
+    int* userfd = (int*)args;
+    printf("Connect: %d\n", *userfd);
+    char msg[SIZE_MSG];
+
+    while (1){
+        printf("I alive %d", *userfd);
+        if(recv(*userfd, msg, sizeof(msg), 0) > 0){
+            printf("Send: %d\n\n", *userfd);
+            for(int i = 0; i <= connect_users; i++){
+                if(users_sockfd[i] != *userfd){
+                    send(users_sockfd[i], msg, sizeof(msg), 0);
+                }
+            }
+
+            bzero(msg, SIZE_MSG);
+        }else{
+            printf("Delete: %d\n\n");
+            delete_userfd(*userfd);
+            break;
+        }
+    }
+    
+}
+
+int main(int argc, char *argv[]) {
+
+	int serv_sockfd, clie_sockfd;
+	socklen_t clilen;
+	struct sockaddr_in serv_addr, cli_addr;
+	int n;
+	serv_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+
+
+
+	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	
+    serv_addr.sin_port = htons(3425);
+	if (bind(serv_sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+		error("Error");
+	listen(serv_sockfd, 5);
+	clilen = sizeof(cli_addr);
+    pthread_t tid;
+
+    printf("Server started\n");
+
+    while (1){
+        int new_connect = accept(serv_sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        
+        if(new_connect == 0){
+            printf("Error accept connect");
+            break;
+        }else{
+            printf("Client connected!\n");
+            printf("UserFD: %d\n", new_connect);
+            printf("Users online: %d\n\n", connect_users);
+            pthread_create(&pthid[connect_users], NULL, server_handler, (void *)&new_connect);
+            pthread_detach(pthid[connect_users]);
+            users_sockfd[connect_users] = new_connect;
+            connect_users++;
+        }
+        
+    }
+
+	close (serv_sockfd);
+	return 0;
+}
